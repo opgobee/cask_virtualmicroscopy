052f56c253236a36bfa1d0bad723860977b0ebca
opgobee <opgobee@6eb45ff1-ddbd-e011-b93f-00151724eb0a>
2013-02-20 10:24:00 +0000

    Remove slides/kidney2 from repository
    git-svn-id: https://svn1.sliksvn.com/anatomypro/Virtual%20microscopy@693 6eb45ff1-ddbd-e011-b93f-00151724eb0a

--- slides/kidney2/labels.js
+++
@@ -1,6 +0,0 @@
-
-var jsLabels = {
-	"label1" :	{"label": "1", "popUpText": "Hoe heet deze uitpuilende 'bobbel'? Zoom uit.", "x": "0.37", "y": "0.628"},
-	"label2" :	{"label": "2", "popUpText": "Hoe heet deze holte? Zoom in.", "x": "0.351", "y": "0.689"},
-	"label3" :	{"label": "3", "popUpText": "Wat is dit ronde balletje? Zoom in.", "x": "0.843", "y": "0.692"}
-};
