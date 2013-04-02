8187657bc57a7b1f290833a7ba4377356913d961
opgobee <opgobee@6eb45ff1-ddbd-e011-b93f-00151724eb0a>
2013-04-02 02:22:03 +0000

    practicum gonaden toegevoegd
    git-svn-id: https://svn1.sliksvn.com/anatomypro/Virtual%20microscopy@729 6eb45ff1-ddbd-e011-b93f-00151724eb0a

--- slides/menus.js
+++ slides/menus.js
@@ -32,7 +32,11 @@ var menus= {
 			"sets":[
 				{title:"Kidney",slides:"090_nier_mens_adult_he_magn2,091_nier_mens_foetaal_he,091_nier_mens_foetaal_he_01_magn2,092_nier_muis_sdh"},
 				{title:"Ureter",slides:"062_ureter_mens_adult_azan,062_ureter_mens_foetaal_azan"},
-				{title:"Bladder",slides:"013_blaas_fret_of_rat_azan"}
+				{title:"Bladder",slides:"013_blaas_fret_of_rat_azan"},
+				{title:"Uterus",slides:"087_uterus_mens_he_magn2,087_uterus_mens_he_02_magn2,088_uterus_mens_he_magn2,088_uterus_mens_he_02_magn2,089_uterus_mens_he_01_magn2,089_uterus_mens_he_03"},
+				{title:"Ovary",slides:"083_ovarium_mens_foetaal_he_01,084_ovarium_kat_he_02,084_ovarium_kat_he_03,085_ovarium_mens_he_01_magn4,085_ovarium_mens_he_02_magn4,085_ovarium_mens_he_02_part1,085_ovarium_mens_he_02_part2,085_ovarium_mens_he_02_part3,086_ovarium_varken_he_magn2"},
+				{title:"Testis",slides:"077_testis_mens_neonaat_he_01,078_testis_muis_he_01"},
+				{title:"Ductus Deferens",slides:"081_ductus_deferens_mens_azan"}
 				]	
 		},
 		{
@@ -56,6 +60,12 @@ var menus= {
 				{title:"Microscopie Vasculair Systeem",slides:"119a_musculeuze_arterie_mens_he,119b_musculeuze_arterie_mens_azan,119c_musculeuze_arterie_mens_rf,119d_musculeuze_arterie_mens_vwf,119e_musculeuze_arterie_mens_1a4,119f_musculeuze_arterie_mens_s100,120a_aorta_foetus_mens_he,120b_aorta_foetus_mens_azan,120b_aorta_foetus_mens_azan_01,120c_aorta_foetus_mens_rf,120d_aorta_foetus_mens_vwf,120e_aorta_foetus_mens_1a4,120f_aorta_foetus_mens_s100,121a_aorta_adult_mens_he,121c_aorta_adult_mens_rf,121d_aorta_adult_mens_vwf,121e_aorta_adult_mens_1a4,121f_aorta_adult_mens_s100,122a_vena_cava_inferior_mens_he,122c_vena_cava_inferior_mens_rf,122d_vena_pulmonalis_mens_vwf,122e_vena_pulmonalis_mens_1a4,122f_vena_pulmonalis_mens_s100"},
 				{title:"Microscopie Longen",slides:"093a_trachea_mens_he,093b_trachea_mens_azan_01,093b_trachea_mens_azan_02,094a_long_varken_azan,094b_long_varken_rf,095_long_muis_g6pdh"},
 				{title:"Microscopie Nieren",slides:"013_blaas_fret_of_rat_azan,062_ureter_mens_adult_azan,062_ureter_mens_foetaal_azan,090_nier_mens_adult_he_magn2,091_nier_mens_foetaal_he,091_nier_mens_foetaal_he_01_magn2,092_nier_muis_sdh"}
+				]
+	    },
+	    {
+			"header":{title:"LUMC - gnk1 - Sturing en Stofwisseling",slides:""},
+			"sets":[
+				{title:"Microscopie Gonaden",slides:"077_testis_mens_neonaat_he_01,078_testis_muis_he_01,081_ductus_deferens_mens_azan,083_ovarium_mens_foetaal_he_01,084_ovarium_kat_he_02,084_ovarium_kat_he_03,085_ovarium_mens_he_01_magn4,085_ovarium_mens_he_02_magn4,085_ovarium_mens_he_02_part1,085_ovarium_mens_he_02_part2,085_ovarium_mens_he_02_part3,086_ovarium_varken_he_magn2,087_uterus_mens_he_magn2,087_uterus_mens_he_02_magn2,088_uterus_mens_he_magn2,088_uterus_mens_he_02_magn2,089_uterus_mens_he_01_magn2,089_uterus_mens_he_03"}
 				]
 	    },
 	    {
