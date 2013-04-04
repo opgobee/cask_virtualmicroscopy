a8b2f872d9e263a39449c22b881b21648d2cc190
opgobee <opgobee@6eb45ff1-ddbd-e011-b93f-00151724eb0a>
2013-04-04 13:41:43 +0000

    slide 4qw_rot (brain) toegevoegd aan standaard, versies van menus, slides en views_on_downloadable toegevoegd
    git-svn-id: https://svn1.sliksvn.com/anatomypro/Virtual%20microscopy@739 6eb45ff1-ddbd-e011-b93f-00151724eb0a

---
+++ slides/menus_on_downloadable_haal_dit_weg.js
@@ -0,0 +1,33 @@
+;
+
+var menus= {
+	"menuAnatomicalRegions":
+	[
+		{
+			"header":{title:"CardioVascular System",slides:""},
+			"sets":[
+				{title:"Muscular arteries",slides:"view=carotis1"}
+				]
+		},
+		{
+			"header":{title:"Nervous System",slides:""},
+			"sets":[
+				{title:"Brain",slides:"4qw_rot"}
+				]
+		},
+		{
+			"header":{title:"ALL SLIDES",slides:"ALL"}	
+		}
+	],
+	"menuInstitutionsModules":
+	[
+	    {
+			"header":{title:"Your institution name - your module name",slides:""},
+			"sets":[
+				{title:"Demo",slides:"view=carotis1,4qw_rot"}
+				]
+	    }
+	]
+
+};
+	
---
+++ slides/slides_on_downloadable_haal_dit_weg.js
@@ -0,0 +1,11 @@
+;
+var slides={
+"4qw_rot" : {"path":"../slides/4qw_rot/","res":0.789,"width":25347,"height":16946,"thumb":"0-0-0rot.jpg","thumbwidth":198,"thumbheight":132,"credits":"LUMC_Anat","title":"Brain"},
+"P-2" : {"path":"../slides/P-2/","res":0.729,"thumb":"0-0-0rot.jpg","thumbwidth":199,"thumbheight":97,"width":12480,"height":25581,"credits":"NLM","title":"Carotid Artery. Atherosclerosis"}
+};
+
+
+
+/*
+"" : {"path":"../../../../../../../../LARGE/virtualmicroscopy/slides//","res":0.2325,"width":,"height":,"credits":"LUMC_Anat","title":""},
+*/
