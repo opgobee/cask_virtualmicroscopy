eb0b4a8db8f0daea656346542bdfb5bad8369034
opgobee <opgobee@6eb45ff1-ddbd-e011-b93f-00151724eb0a>
2013-05-07 19:50:59 +0000

    view carotis geupdate in views on downloadable origtiledviewer.js weg (staat nu in ARCHIVE)
    git-svn-id: https://svn1.sliksvn.com/anatomypro/Virtual%20microscopy@757 6eb45ff1-ddbd-e011-b93f-00151724eb0a

--- slides/views_on_downloadable_haal_dit_weg.js
+++ slides/views_on_downloadable_haal_dit_weg.js
@@ -1,4 +1,4 @@
 ;
 var views = {
-	"carotis1" : "slide=P-2&labels=(0.4051,0.2927,Plaque,Atheromatous+plaque%2C+alsmost+completely+occluding+vascular+lumen)(0.4949,0.3675,Media,)(0.6333,0.4886,Adv,Adventitia)(0.3512,0.4457,Deposits,)(0.3475,0.8264,Plaque,Atheromatous+plaque)(0.6295,0.7951,Int,Intima)(0.668,0.8223,Med,Media)(0.7269,0.8848,Adv,Adventitia)(0.4846,0.7703,Lumen,Lumen+of+vessel)"
+	"carotis1" : "slide=P-2&zoom=2&x=0.5128&y=0.5035&labels=(0.668,0.8223,Med,Media)(0.7269,0.8848,Adv,Adventitia)(0.4846,0.7703,Lumen,Lumen+of+vessel)(0.3512,0.4457,Deposits,)(0.6333,0.4886,Adv,Adventitia)(0.2423,0.3632,Plaque,Atheromatous+plaque%2C+alsmost+completely+occluding+vascular+lumen)(0.5083,0.4351,Plaque,)(0.2571,0.1764,Plaque,)(0.7147,0.2007,Plaque,)(0.5096,0.3272,Cap,)(0.7487,0.4227,Media,)(0.2071,0.872,Plaque,Atheromatous+plaque)(0.5507,0.7262,Int,Intima)"
 }
